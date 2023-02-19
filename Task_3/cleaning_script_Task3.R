340b3e122340f3eb838c8cab31f0edc29d29cf89
euanelrick <euanelrick@gmail.com>
2023-02-19 12:19:47 +0000

    Read in seabirds data

---
+++ Task_3/cleaning_script_Task3.R
@@ -0,0 +1,23 @@
+# Cleaning script
+
+
+# Loading packages --------------------------------------------------------
+
+# install.packages("here")
+
+library(tidyverse)
+
+library(janitor)
+
+library(readxl)
+
+library(here)
+
+# Reading in seabirds data ------------------------------------------------
+
+here()
+
+seabirds_ships_data <- read_xls("raw_data/seabirds.xls", sheet = 1)
+
+seabirds_birds_data <- read_xls("raw_data/seabirds.xls", sheet = 2)
+
